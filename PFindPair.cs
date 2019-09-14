484d7efdd1162ffb955f510f24e90233ad3fa2c3
wesam ziadat <wesam.ziadat@yahoo.com>
2019-09-14 05:25:08 +0300

    init commit

---
+++ PFindPair.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+
+class PFindPair
+{
+    static void Main(string[] args)
+    {
+
+        int[] randomNumbers = new int[] { 1, 9, 7, 11, 4, 8, 5, 2, 22, 3, 4, 6, 10 };
+        int left = 0;
+        int right = randomNumbers.Length - 1;
+        int sum = 13;
+
+        quickSort(randomNumbers, left, right);
+
+        foreach (int item in randomNumbers)
+        {
+            Console.WriteLine("Sorted Array {0}", item);
+        }
+
+        findPair(randomNumbers, sum);
+
+    }
+
+    static public void quickSort(int[] arr, int left, int right)
+    {
+
+        if (left < right)
+        {
+            int pi = partition(arr, left, right);
+
+            quickSort(arr, left, pi - 1);
+            quickSort(arr, pi + 1, right);
+        }
+
+    }
+    static public int partition(int[] arr, int left, int right)
+    {
+        int pivot = arr[right];
+        int i = left - 1;
+
+
+        for (int j = left; j < right; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp1 = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp1;
+
+        return i + 1;
+
+    }
+    static public void findPair(int[] randomNumbers, int sum)
+    {
+        int low = 0;
+        int high = randomNumbers.Length - 1;
+
+        List<String> listOfPairs = new List<String>();
+
+        while (low < high)
+        {
+            if (randomNumbers[low] + randomNumbers[high] == sum)
+            {
+
+                Console.WriteLine("the pair of {0} and {1} sum up to {2}", randomNumbers[low], randomNumbers[high], sum);
+                low++;
+                high--;
+            }
+            else if (randomNumbers[low] + randomNumbers[high] < sum)
+            {
+                low++;
+            }
+            else
+            {
+                high--;
+            }
+        }
+
+        foreach (string item in listOfPairs)
+        {
+            Console.Write("sum of  {0} -", item);
+        }
+
+    }
+}
---
+++ PGetRepeatedNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+    class PGetRepeatedNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersList = new int[] { 1, 0, 6, 9, 7, 5, 3, 6, 0, 8, 1 };
+
+            int size = numbersList.Length;
+
+            Console.WriteLine("size  = {0}", size);
+
+            GetRepeatedNumbers(numbersList, size);
+        }
+
+        static void GetRepeatedNumbers(int[] arr, int size)
+        {
+            int i;
+            int[] count = new int[size];
+
+            Console.WriteLine("the repeated numbers are :");
+
+            for (i = 0; i < size; i++)
+            {
+                if (count[arr[i]] == 1)
+                {
+                    Console.Write("{0} /", arr[i]);
+                }
+                else
+                {
+                    count[arr[i]]++;
+                }
+            }
+
+        }
+    }
