002fb25d8717d74c1e8490b38cbdd5776bc27ee8
wesam ziadat <wesam.ziadat@yahoo.com>
2019-09-18 23:02:52 +0300

    Adding element wise array summation

--- PSumArrayElementWise.cs
+++ PSumArrayElementWise.cs
@@ -4,6 +4,42 @@ class PSumArrayElementWise
 {
     public static int[] Process(int[] arr1, int[] arr2)
     {
+
+        int[] inputArray = new int[] {-1, -2};
+
+        int max = inputArray[0] * inputArray[1];
+        int secound = 1;
+
+
+        for (int i = 0; i < inputArray.Length - 1; i++)
+        {
+            int first = inputArray[i];
+
+            if ((0 < i + 1) && (i + 1 <= inputArray.Length - 1))
+            {
+
+                secound = inputArray[i + 1];
+
+                if (first * secound > max)
+                {
+                    max = first * secound;
+                }
+
+
+            }
+
+            if (inputArray.Length <= 2)
+            {
+                max = Math.Max(first, secound);
+            }
+
+
+        }
+
+
+
+
+
         int sum = 0;
         int len1 = 0;
         int len2 = 0;
@@ -13,17 +49,17 @@ class PSumArrayElementWise
         if (arr1.Length > arr2.Length) { size = arr1.Length; }
         else { size = arr2.Length; }
 
-        int[] RepArray = new int[size+1];
+        int[] RepArray = new int[size + 1];
 
         for (int i = 0; i < size; i++)
         {
             //output = (val % 2 == 1) ? "Number is odd" : "Number is even";
-            sum = ( (len1 - i >= 0) ? arr1[len1 - i] : 0) + ( (len2 - i >= 0) ? arr2[len2 - i] : 0) + carry;
+            sum = ((len1 - i >= 0) ? arr1[len1 - i] : 0) + ((len2 - i >= 0) ? arr2[len2 - i] : 0) + carry;
 
             RepArray[(size - i)] = sum % 10;
             carry = sum / 10;
         }
-       // RepArray[0] = carry;
+        // RepArray[0] = carry;
         return RepArray;
 
 
